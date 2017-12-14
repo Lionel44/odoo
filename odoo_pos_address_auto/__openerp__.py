20fff1d3597f6199dfce896f036fff28e082d4fa
Lionel Blanchet <34477453+Lionel44@users.noreply.github.com>
2017-12-14 14:45:37 +0100

    Update __openerp__.py

--- odoo_pos_address_auto/__openerp__.py
+++ odoo_pos_address_auto/__openerp__.py
@@ -58,7 +58,7 @@ Par exemple en tapant seulement "Moza 44130" dans le champ Rue, l'autocompletion
     ],
     "demo": [
     ],
-    "images": ["static/description/address_auto1.jpg"
+    "images": ["static/description/Address_auto1.jpg"
     ],
     "data": [
 	     "views/templates.xml"
