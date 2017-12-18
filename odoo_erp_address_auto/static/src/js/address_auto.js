20ada24ad24da0ddee4f1ff1ef6e75edde66c420
Lionel Blanchet <34477453+Lionel44@users.noreply.github.com>
2017-12-18 18:52:12 +0100

    Update address_auto.js

--- odoo_erp_address_auto/static/src/js/address_auto.js
+++ odoo_erp_address_auto/static/src/js/address_auto.js
@@ -1,46 +1,52 @@
 odoo.define('odoo_erp_address_auto', function (require) {
 "use strict";
-
-// autocompletion de l'adresse
-        this.$('.o_address_street').autocomplete({
-	source: function (request, response) {
-		$.ajax({
-			url: "https://api-adresse.data.gouv.fr/search/?postcode="+$('.client-address-zip').val(),
-			data: { q: request.term, limit: 50},
-			dataType: "json",
-			success: function (data) {
-				response($.map(data.features, function (item) {
-					return { label: item.properties.name + " | " +  item.properties.postcode + " - " + item.properties.city,
-						city: item.properties.city,
-        				postcode: item.properties.postcode,
-					 value: item.properties.name};
-				}));
-			}
+view.include({init:function(parent,dataset,options){
+    self=this;
+    console.log(options._model,dataset._model['name'],"init");
+    this._super.apply(this, arguments);
+    if (dataset._model['name'] == 'res.partner'){
+	
+	
+		// autocompletion de l'adresse
+		this.$('.o_address_street').autocomplete({
+		source: function (request, response) {
+			$.ajax({
+				url: "https://api-adresse.data.gouv.fr/search/?postcode="+$('.client-address-zip').val(),
+				data: { q: request.term, limit: 50},
+				dataType: "json",
+				success: function (data) {
+					response($.map(data.features, function (item) {
+						return { label: item.properties.name + " | " +  item.properties.postcode + " - " + item.properties.city,
+							city: item.properties.city,
+							postcode: item.properties.postcode,
+						value: item.properties.name};
+					}));
+				}
+			});
+		},
+		// On remplit aussi la ville et le code postal
+		select: function(event, ui) {
+				$('.o_address_city').val(ui.item.city);
+				$('.o_address_zip').val(ui.item.postcode);
+			}            
 		});
-	},
-    // On remplit aussi la ville et le code postal
-    select: function(event, ui) {
-     		$('.o_address_city').val(ui.item.city);
-       		$('.o_address_zip').val(ui.item.postcode);
-    }            
-        });
 
-// autocompletion du code postal
-        this.$('.o_address_zip').autocomplete({
-        	source: function (request, response) {
-        		$.ajax({
-        			url: "https://api-adresse.data.gouv.fr/search/?postcode="+$('.client-address-zip').val(),
-        			data: { q: request.term, limit: 50},
-        			dataType: "json",
-        			success: function (data) {
-        				var postcodes = [];
-        				response($.map(data.features, function (item) {
-        					// Ici on est obligÃ© d'ajouter les CP dans un array pour ne pas avoir plusieurs fois le mÃªme
-        					if ($.inArray(item.properties.city, postcodes) == -1) {
-        						postcodes.push(item.properties.city);
-        						return { label: item.properties.postcode + " - " + item.properties.city, 
-        								 city: item.properties.city,
-        								 value: item.properties.postcode
+		// autocompletion du code postal
+		this.$('.o_address_zip').autocomplete({
+        source: function (request, response) {
+        	$.ajax({
+        		url: "https://api-adresse.data.gouv.fr/search/?postcode="+$('.client-address-zip').val(),
+        		data: { q: request.term, limit: 50},
+        		dataType: "json",
+        		success: function (data) {
+        			var postcodes = [];
+        			response($.map(data.features, function (item) {
+        				// Ici on est obligÃ© d'ajouter les CP dans un array pour ne pas avoir plusieurs fois le mÃªme
+        				if ($.inArray(item.properties.city, postcodes) == -1) {
+        					postcodes.push(item.properties.city);
+        					return {label: item.properties.postcode + " - " + item.properties.city, 
+									city: item.properties.city,
+        							 value: item.properties.postcode
         						};
         					}
         				}));
@@ -53,7 +59,7 @@ odoo.define('odoo_erp_address_auto', function (require) {
 	       }            
         });
 
-// autocompletion de la ville
+	// autocompletion de la ville
         this.$('.o_address_city').autocomplete({
         	source: function (request, response) {
         		$.ajax({
@@ -81,3 +87,7 @@ odoo.define('odoo_erp_address_auto', function (require) {
         	}
     });
 });
+}
+}
+}
+);
